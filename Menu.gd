33c2b88a6a025eff44c6ab2baa655c3f8917e883
Andrey Arehov <programmirovanie.io@gmail.com>
2020-07-13 08:01:52 +0300

    Commit

--- Menu.gd
+++ Menu.gd
@@ -1,10 +1,11 @@
 extends Node2D
 
 func _ready():
-	$Play.connect('pressed', self, "btn_new_game")
+	$Play.connect('pressed', self, "game")
 	$Exit.connect('pressed', self, "btn_exit")
 
-func btn_new_game():
+func game():
 	get_tree().change_scene("res://Level1.tscn")
+
 func btn_exit():
 	get_tree().quit()
--- Rock.gd
+++ Rock.gd
@@ -14,6 +14,5 @@ func _physics_process(delta):
 func _on_VisibilityNotifier2D_screen_exited():
 	queue_free()
 
-
 func _on_Rock_body_entered(body):
 	queue_free()
