db099787ab2166b88a72dc459029deef59d806ed
Andrey Arehov <programmirovanie.io@gmail.com>
2020-07-13 11:24:27 +0300

    Initial Commit!

--- Menu.gd
+++ Menu.gd
@@ -1,10 +1,10 @@
 extends Node2D
 
 func _ready():
-	$Play.connect('pressed', self, "game")
-	$Exit.connect('pressed', self, "btn_exit")
+	$Play.connect("pressed", self, "btn_new_game")
+	$Exit.connect("pressed", self, "btn_exit")
 
-func game():
+func btn_new_game():
 	get_tree().change_scene("res://Level1.tscn")
 
 func btn_exit():
--- Rock.gd
+++ Rock.gd
@@ -3,13 +3,14 @@ extends Area2D
 const SPEED = 500
 export var direction = 1
 var velocity = Vector2()
+var GRAVITY = 0.10
 
 func _physics_process(delta):
 	velocity.x = SPEED * delta * direction
+	velocity.y += GRAVITY
 	if(direction == -1):
 		$Sprite.flip_h = true
 	translate(velocity)
-
 
 func _on_VisibilityNotifier2D_screen_exited():
 	queue_free()
