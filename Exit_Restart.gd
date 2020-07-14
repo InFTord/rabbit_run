e18f3746f1eedf71a8f771b921f96c6c8e465664
Andrey Arehov <programmirovanie.io@gmail.com>
2020-07-14 15:47:42 +0300

    Добавил две кнопки - выход в меню и перезапуск)

---
+++ Exit_Restart.gd
@@ -0,0 +1,11 @@
+extends Node2D
+
+func _ready():
+	$CanvasLayer/Exit.connect('pressed',self, "btn_menu")
+	$CanvasLayer2/Restart.connect('pressed',self, "btn_restart")
+
+func btn_menu():
+	get_tree().change_scene("res://Menu.tscn")
+
+func btn_restart():
+	get_tree().change_scene("res://Level1.tscn")
